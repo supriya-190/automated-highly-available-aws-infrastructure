b2a28bbe79cdb22719d89988912a53b37569c30d
supriya-190 <supriyaanuchuri11@gmail.com>
2026-08-17 11:17:30 +0530

    Add VPC CIDR variable to Terraform configuration

--- terraform/terraform/variables.tf
+++ terraform/terraform/variables.tf
@@ -3,3 +3,8 @@ variable "aws_region" {
   type        = string
   default     = "ap-south-1"
 }
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
