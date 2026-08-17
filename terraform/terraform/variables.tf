d490e54848773cc0b3dc3488f4416f78ab286de0
supriya-190 <supriyaanuchuri11@gmail.com>
2026-08-17 11:40:04 +0530

    Add alert_email variable for infrastructure alerts

--- terraform/terraform/variables.tf
+++ terraform/terraform/variables.tf
@@ -8,3 +8,8 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+variable "alert_email" {
+  description = "Email address for infrastructure alerts"
+  type        = string
+  sensitive   = true
+}
