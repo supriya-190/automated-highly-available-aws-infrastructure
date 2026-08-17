e09195e0383e3cca0650137e00d362cd66f577f6
supriya-190 <supriyaanuchuri11@gmail.com>
2026-08-17 11:31:49 +0530

    Add ALB and target group resources in alb.tf

---
+++ terraform/terraform/terraform/alb.tf
@@ -0,0 +1,51 @@
+resource "aws_lb_target_group" "app" {
+  name     = "devops-app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "devops-app-target-group"
+  }
+}
+
+resource "aws_lb" "app" {
+  name               = "devops-app-alb"
+  internal           = false
+  load_balancer_type = "application"
+
+  security_groups = [
+    aws_security_group.alb.id
+  ]
+
+  subnets = [
+    aws_subnet.public_1.id,
+    aws_subnet.public_2.id
+  ]
+
+  tags = {
+    Name = "devops-application-load-balancer"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
